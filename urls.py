1f733f48255b9f69ba919f76027a06a008d27862
Erhan KESKEN <erhan@kesken.org>
2012-02-11 18:54:47 +0200

    added redirect to accounts/profile, due to #1. fixed broken html templates.

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url, include
+from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('views',
-    # url(r'^accounts/profile/$', 'redirect_to', {'url': '/'}),
+    url(r'^accounts/profile/$', redirect_to, {'url': '/'}),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^setlang$', 'setlang'),
     url(r'.*', 'default')
